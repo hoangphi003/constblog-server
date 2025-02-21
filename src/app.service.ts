4437e0143637aad68725fff51ba77e963dd983b9
Your Name <jamesnguyen.devs@gmail.com>
2025-02-21 10:48:43 +0700

    add text

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Docker pushed @@';
+    return 'Writebox is a text editor designed with simplicity and distraction-free writing. While many applications tend to become feature-rich and complex over time, Writebox takes a different approach. Writebox continues to focus on the essential features required for writing on a computer, providing an environment that allows writers to concentrate without unnecessary distractions.Writebox was developed and released by @shibuya';
   }
 }
